7e784f1b782f193a24444cc8905c244b21fa5ab3
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 17:06:31 -0300

    add Metadata, use generateMetadata

--- restaurancy/src/app/[id]/page.tsx
+++ restaurancy/src/app/[id]/page.tsx
@@ -1,6 +1,15 @@
 import api from "@/api";
 import RestaurantCard from "../components/RestaurantCard";
 
+export async function generateMetadata({params: {id}}: {params: {id: string}}) {
+  const restaurant = await api.fetch(id);
+
+  return {
+    title: `${restaurant.name} - Restaurancy`,
+    description: restaurant.description,
+  };
+}
+
 export default async function RestaurantPage({ params: { id } }: { params: { id: string } }) {
   const restaurant = await api.fetch(id);
   return <RestaurantCard restaurant={restaurant} />
--- restaurancy/src/app/layout.tsx
+++ restaurancy/src/app/layout.tsx
@@ -4,8 +4,10 @@ import "./globals.css";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Restaurancy",
-};
+  title: 'Restaurancy - Next.js App',
+  description: 'The best restaurants in the world',
+  keywords: ['restaurant', 'food', 'eat', 'dinner', 'lunch'],
+}
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
