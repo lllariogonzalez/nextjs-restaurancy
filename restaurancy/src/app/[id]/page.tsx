ac6040ad36fa34e49fb894fcc4f2bb67ed3cdf7d
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 18:06:38 -0300

    add generateStaticParams for compile

--- restaurancy/src/app/[id]/page.tsx
+++ restaurancy/src/app/[id]/page.tsx
@@ -9,6 +9,14 @@ export async function generateMetadata({params: {id}}: {params: {id: string}}) {
     description: restaurant.description,
   };
 }
+// con esta función le decimos a Next que rutas generar en tiempo de compilación
+export async function generateStaticParams() {
+  const restaurants = await api.list();
+ 
+  return restaurants.map((restaurant) => ({
+    id: restaurant.id,
+  }));
+}
 
 export default async function RestaurantPage({ params: { id } }: { params: { id: string } }) {
   const restaurant = await api.fetch(id);
