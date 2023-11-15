aa1eb5a61c160a7106ccde1b3fe8ac711067134f
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 17:45:58 -0300

    fix add key props to RestaurantCard

--- restaurancy/src/app/page.tsx
+++ restaurancy/src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
-      {restaurants.map((restaurant) => <RestaurantCard restaurant={restaurant} />)}
+      {restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)}
     </section>
   );
 }
