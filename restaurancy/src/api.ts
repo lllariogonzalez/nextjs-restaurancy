1a30770dd13207f224f73a61c6e6deed09dc05c6
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 17:36:20 -0300

    add GoogleSheets  .csv as database

--- restaurancy/src/api.ts
+++ restaurancy/src/api.ts
@@ -126,10 +126,27 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const api = {
   list: async (): Promise<Restaurant[]> => {
-    await sleep(750);
+    // Obtenemos la información de Google Sheets en formato texto y la dividimos por líneas, nos saltamos la primera línea porque es el encabezado
+    const [, ...data] = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSTJoaW5Ti0T8bPH70oVgd-aIuJVjYqFr9uk7AuDu0IkAZ5-8_b6pjuprTicomYCllCbWGEVQl-oJ0v/pub?output=csv').then(res => res.text()).then(text => text.split('\n'))
 
+    // Convertimos cada línea en un objeto Restaurant, asegúrate de que los campos no posean `,`
+    const restaurants: Restaurant[] = data.map((row) => {
+      const [id, name, description, address, score, ratings, image] = row.split(',')
+      return {
+        id,
+        name,
+        description,
+        address,
+        score: Number(score),
+        ratings: Number(ratings),
+        image
+      }
+    })
+
+    // Lo retornamos
     return restaurants;
   },
+
   fetch: async (id: Restaurant["id"]): Promise<Restaurant> => {
     await sleep(750);
 
