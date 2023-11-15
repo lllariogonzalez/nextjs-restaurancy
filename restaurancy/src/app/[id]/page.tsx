44513ce67d8cb48e4bf47fd3e5aa2aad5e03280d
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 13:28:29 -0300

    add restaurant details by id, use dynamic route

---
+++ restaurancy/src/app/[id]/page.tsx
@@ -0,0 +1,24 @@
+import api from "@/api";
+
+export default async function RestaurantPage({ params: { id } }: { params: { id: string } }) {
+  const restaurant = await api.fetch(id);
+
+  return (
+    <article key={restaurant.id}>
+      <img
+        alt={restaurant.name}
+        className="mb-3 h-[300px] w-full object-cover"
+        src={restaurant.image}
+      />
+      <h2 className="inline-flex gap-2 text-lg font-bold">
+        <span>{restaurant.name}</span>
+        <small className="inline-flex gap-1">
+          <span>⭐</span>
+          <span>{restaurant.score}</span>
+          <span className="font-normal opacity-75">({restaurant.ratings})</span>
+        </small>
+      </h2>
+      <p className="opacity-90">{restaurant.description}</p>
+    </article>
+  );
+}
