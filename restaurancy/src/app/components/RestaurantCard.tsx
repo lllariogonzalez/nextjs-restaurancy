ffeeaaf3f7123da9685a730fcecc8da135c471cf
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 14:08:46 -0300

    add component RestaurantCard

---
+++ restaurancy/src/app/components/RestaurantCard.tsx
@@ -0,0 +1,22 @@
+import { Restaurant } from "@/api";
+
+export default function RestaurantCard({restaurant}: {restaurant: Restaurant}){
+  return (
+    <article key={restaurant.id}>
+      <img
+        alt={restaurant.name}
+        className="mb-3 h-[300px] w-full object-cover"
+        src={restaurant.image}
+      />
+      <h2 className="inline-flex gap-2 text-lg font-bold">
+        <span>{restaurant.name}</span>
+        <small className="inline-flex gap-1">
+          <span>⭐</span>
+          <span>{restaurant.score}</span>
+          <span className="font-normal opacity-75">({restaurant.ratings})</span>
+        </small>
+      </h2>
+      <p className="opacity-90">{restaurant.description}</p>
+    </article>
+  )
+}
