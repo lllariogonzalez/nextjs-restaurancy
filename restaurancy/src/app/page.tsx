6c093861633cccf62625d23e08bfbce46df2012e
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 22:03:52 -0300

    add span not results for searchs

--- restaurancy/src/app/page.tsx
+++ restaurancy/src/app/page.tsx
@@ -2,14 +2,18 @@ import api from "@/api";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchBox from "./components/SearchBox";
 
-export default async function Home() {
-  const restaurants = await api.list();
+export default async function Home({ searchParams }: { searchParams: { q: string } }) {
+  const restaurants = await api.search(searchParams.q);
 
   return (
     <section>
       <SearchBox />
       <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
-        {restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)}
+        {
+          restaurants.length
+            ? restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)
+            : <span>No matching results</span>
+        }
       </section>
     </section>
   );
