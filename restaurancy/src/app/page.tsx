b51919d130298bd1d47362b97f8a2c311fef77bd
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-15 21:49:43 -0300

    add Home page the SearchBox component

--- restaurancy/src/app/page.tsx
+++ restaurancy/src/app/page.tsx
@@ -1,12 +1,16 @@
 import api from "@/api";
 import RestaurantCard from "./components/RestaurantCard";
+import SearchBox from "./components/SearchBox";
 
 export default async function Home() {
   const restaurants = await api.list();
 
   return (
-    <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
-      {restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)}
+    <section>
+      <SearchBox />
+      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
+        {restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)}
+      </section>
     </section>
   );
 }
